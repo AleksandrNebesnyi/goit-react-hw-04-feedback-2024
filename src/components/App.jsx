53770d51a6bcf206c99a3197f998d30cd208c8c0
AleksandrNebesnyi <a.nebesnyi@gmail.com>
2024-05-03 13:14:04 +0200

    relase base task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,67 +1,76 @@
-import { Component } from 'react'
+import { useState } from 'react';
 import { Title } from './Title';
-import css from './App.module.css'
+import css from './App.module.css';
 
+export const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-export class App extends Component {
-state ={
-  good: 0,
-  neutral: 0,
-  bad: 0
-};
+  const options = Object.keys({ good, neutral, bad });
 
-onBtnClick =e=>{
-const keyName=e.currentTarget.name;
-this.setState(prevState=>{
-  return {
-    [keyName]:prevState[keyName]+1
-  }
-})
-};
+  const onBtnClick = e => {
+    const keyName = e.currentTarget.name;
 
- makeButton =(options)=>{
-  return options.map(option=>(
-  <button key={option} className={css.button}type='button' name={option} onClick={this.onBtnClick}>{option}</button>))
- };
+    switch (keyName) {
+      case 'good':
+        setGood(good + 1);
+        break;
+      case 'neutral':
+        setNeutral(neutral + 1);
+        break;
+      case 'bad':
+        setBad(bad + 1);
+        break;
+      default:
+        break;
+    }
+  };
 
+  const makeButton = items => {
+    return items.map(item => (
+      <button
+        key={item}
+        className={css.button}
+        type="button"
+        name={item}
+        onClick={onBtnClick}
+      >
+        {item}
+      </button>
+    ));
+  };
 
- countTotalFeedback=()=>{
-  return Object.values(this.state).reduce((acc,currentItem)=>acc+currentItem,0)
- };
- countPositiveFeedbackPercentage=()=>{
-  const { good } = this.state;
+  const countTotalFeedback = () => {
+    return good + neutral + bad;
+  };
+  const countPositiveFeedbackPercentage = () => {
     if (good === 0) return 0;
-    return Math.floor((good / this.countTotalFeedback()) * 100);
+    return Math.floor((good / countTotalFeedback()) * 100);
+  };
+  return (
+    <div className={css.container}>
+      <Title text="Please leave fidback">
+        <div className={css.btnContainer}>{makeButton(options)}</div>
+      </Title>
 
- }
-
-render() { 
- const  {good,neutral,bad}=this.state; 
- const  options = Object.keys(this.state);
-
-return(
-<div className={css.container} >
-
-<Title text='Please leave fidback'>
-  <div className={css.btnContainer}>
-  {this.makeButton(options)}  
-  </div>
-</Title> 
-
-<Title text='Statistics'>
-<div>
-{this.countTotalFeedback()<=0 ? <p>No feedback given</p> : <>
-<p className={css.statisticItem}>Good:{good}</p>
-  <p className={css.statisticItem}>Neutral:{neutral}</p>
-  <p className={css.statisticItem}>Bad:{bad}</p>
-  <p className={css.statisticItem}>Total:{(this.countTotalFeedback())}</p>
-  <p className={css.statisticItem}>Positiv Feedback:{this.countPositiveFeedbackPercentage()}</p>
-</>}
-   
-  </div>
-</Title>
-  </div>
-)
-
-}
+      <Title text="Statistics">
+        <div>
+          {countTotalFeedback() <= 0 ? (
+            <p>No feedback given</p>
+          ) : (
+            <>
+              <p className={css.statisticItem}>Good:{good}</p>
+              <p className={css.statisticItem}>Neutral:{neutral}</p>
+              <p className={css.statisticItem}>Bad:{bad}</p>
+              <p className={css.statisticItem}>Total:{countTotalFeedback()}</p>
+              <p className={css.statisticItem}>
+                Positiv Feedback:{countPositiveFeedbackPercentage()}
+              </p>
+            </>
+          )}
+        </div>
+      </Title>
+    </div>
+  );
 };
--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,9 +1,28 @@
-export const Title = ({text,children})=>{
-    return(
-        <>
-        <h2 className="">{text}</h2>
-        {children}
-        </>
+import React from 'react';
+import { ReactNode } from 'react'; // Імпортуйте React і тип ReactNode// Імпортуйте React і тип ReactNode
 
-    )
+interface TitleProps {
+  text: string;
+  children: ReactNode;
 }
+
+export const Title: React.FunctionComponent<TitleProps> = ({
+  text,
+  children,
+}) => {
+  return (
+    <>
+      <h2 className="">{text}</h2>
+      {children}
+    </>
+  );
+};
+
+// export const Title = ({ text, children }) => {
+//   return (
+//     <>
+//       <h2 className="">{text}</h2>
+//       {children}
+//     </>
+//   );
+// };
